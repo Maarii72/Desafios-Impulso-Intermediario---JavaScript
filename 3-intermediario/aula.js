6dceebb83ff7528e3da6e39cda9fb6abd74f1e7d
Marilena Sales <marilena.sales@gmail.com>
2022-05-17 15:58:09 -0300

    fim 3-intermediario

--- 3-intermediario/aula.js
+++ 3-intermediario/aula.js
@@ -1,29 +1,27 @@
-/*
-let x = parseInt(gets());
-let y = parseFloat(gets());
-*/
-//TODO: Complete os espaços em branco com uma possível resolução para o problema.
 for(let j = 0; j<3;j++)
 {
   let number = parseInt(gets());
   let velocity = gets().split(" ");
+  //let number = 10;
+  //let velocity = [10, 10, 10, 10, 15, 18, 20, 15, 11, 10];
   let max = 0;
   let level = 0;
 
   for(i = 0 ; i < number; i++){
     if(max < parseInt(velocity[i])){
-      max = parseInt(        );
+      max = parseInt(velocity[i]);
     }
   }
   
-  if(max <  ){
+  if(max < 10){
     level = 1;
   }
-  else if(max >=  && max < ){
+  else if(max >= 10 && max < 20){
     level = 2;
   } 
   else{
     level = 3;
   }
-  print(   );
+  
+  console.log(level);
 }
---
+++ 4-intermediario/aula.js
@@ -0,0 +1,22 @@
+//TODO: Complete os espaços em branco com uma possível solução para o problema.
+
+const array = [];
+const isUnique = [];
+let count = 0;
+
+do {
+  const value = gets();
+  if (value == '') {
+    break;
+  } else {
+    array.push(       );
+  }
+} while (true);
+
+for (item       array) {
+  if (!isUnique.includes(item)) isUnique.push(     );
+}
+
+const result = isUnique.     ;
+
+print(result);
